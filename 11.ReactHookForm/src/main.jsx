04e6a9873422796009cb02d83c0087736c058baf
sahilvish01 <sahilvish6542@gmail.com>
2026-06-28 20:19:31 +0530

    React Hook Form + Zod + regex Expression

---
+++ 11.ReactHookForm/src/main.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+import Form1 from './Form1.jsx'
+import Form2 from './Form2.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+    <Form2 />
+
+)
